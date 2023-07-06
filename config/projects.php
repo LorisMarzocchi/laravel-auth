f4a7f5a8d00c4f4344380796c090e7c6f59ab288
LorisMarzocchi <ml77182a@gmail.com>
2023-07-07 00:48:02 +0200

    addProject&UsersDB

---
+++ config/projects.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    [
+        "title"            => "Html Css Bootstrap Dashboard",
+        "thumb"           => "https://picsum.photos/id/20/200/300",
+        "description"      => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae provident illum sapiente expedita beatae.",
+        "languages"        => "HTML, CSS, BOOTSTRAP",
+        "link_github"      => "https://github.com/LorisMarzocchi/html-css-bootstrap-dashboard",
+    ],
+    [
+        "title"            => "Spotify Web",
+        "thumb"           => "https://picsum.photos/id/1/200/300",
+        "description"      => "A clone of the spotify web interface",
+        "languages"        => "HTML, CSS",
+        "link_github"      => "https://github.com/LorisMarzocchi/html-css-spotifyweb",
+    ],
+    [
+        "title"            => "Js Social Posts",
+        "thumb"           => "https://picsum.photos/id/15/200/300",
+        "description"      => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae provident illum sapiente expedita beatae.",
+        "languages"        => "HTML, CSS, JAVASCRIPT",
+        "link_github"      => "https://github.com/LorisMarzocchi/js-social-posts",
+    ],
+    [
+        "title"            => "Vite Boolflix",
+        "thumb"           => "https://picsum.photos/id/10/200/300",
+        "description"      => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae provident illum sapiente expedita beatae.",
+        "languages"        => "HTML, SCSS, JAVASCRIPT, VUE",
+        "link_github"      => "https://github.com/LorisMarzocchi/vite-boolflix",
+    ],
+    [
+        "title"            => "Project Barber-Shop",
+        "thumb"           => "https://picsum.photos/id/150/200/300",
+        "description"      => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae provident illum sapiente expedita beatae.",
+        "languages"        => "HTML, SCSS, VUE, JAVASCRIPT ",
+        "link_github"      => "https://github.com/LorisMarzocchi/proj-html-vuejs",
+    ],
+    [
+        "title"            => "Laravel DC Comics",
+        "thumb"           => "https://picsum.photos/id/50/200/300",
+        "description"      => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae provident illum sapiente expedita beatae.",
+        "languages"        => "PHP, BLADE ",
+        "link_github"      => "https://github.com/LorisMarzocchi/laravel-dc-comics",
+    ],
+
+
+
+];
--- resources/views/guests/layouts/base.blade.php
+++ resources/views/guests/layouts/base.blade.php
@@ -5,16 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite('resources/js/app.js')
 </head>
 <body>
-    @include('admin.includes.header')
+    @include('guests.includes.header')
     <div class="container">
+
         <main>
             @yield('contents')
         </main>
-
     </div>
-    @include('admin.includes.footer')
-
+    @include('guests.includes.footer')
 </body>
 </html>
